d7012f9a75ae9ee0b555c48f978f1d0c560a9ef1
Robert Hanna <robertdhanna94@gmail.com>
2020-04-12 15:28:40 -0600

    board styling

--- client/src/Board.tsx
+++ client/src/Board.tsx
@@ -8,21 +8,25 @@ type BoardProps = {
 };
 function Board({ game, sendMessage }: BoardProps) {
   console.log(sendMessage);
-  const gridRows = chunk(Object.entries(game.Cards), 5).map((row) => {
-    return (
-      <Grid.Row>
-        {row.map(([cardName]) => {
-          return (
-            <Grid.Column>
-              <Segment textAlign="center">{cardName}</Segment>
-            </Grid.Column>
-          );
-        })}
-      </Grid.Row>
-    );
-  });
+  const gridRows = React.useMemo(() => {
+    return chunk(Object.entries(game.Cards), 5).map((row) => {
+      return (
+        <Grid.Row>
+          {row.map(([cardName]) => {
+            return (
+              <Grid.Column>
+                <Segment textAlign="center" style={{ userSelect: 'none' }}>
+                  {cardName.toLocaleUpperCase()}
+                </Segment>
+              </Grid.Column>
+            );
+          })}
+        </Grid.Row>
+      );
+    });
+  }, [game.Cards]);
   return (
-    <Grid container stackable columns={5}>
+    <Grid stackable columns={5} container celled="internally" style={{ backgroundColor: 'cornflowerblue' }}>
       {gridRows}
     </Grid>
   );
